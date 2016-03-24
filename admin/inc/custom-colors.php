919b8695c707ffd4b619cb968bbfe038523d1f48
abaicus <baicusandrei@me.com>
2016-03-24 22:25:48 +0200

    !!!Added Color options

--- admin/inc/custom-colors.php
+++ admin/inc/custom-colors.php
@@ -1,10 +1,12 @@
 <?php
 function team_plugin_custom_colors() {
 
-$buttons_color =
-$buttons_text_color =
-$links_color =
-$
+$buttons_color = get_theme_mod('team_plugin_buttons_color', '#333');
+$buttons_hover_color = get_theme_mod('team_plugin_buttons_color', '#555');
+$buttons_text_color = get_theme_mod('team_plugin_buttons_text_color', '#fff');
+$buttons_hover_text_color = get_theme_mod('team_plugin_buttons_hover_text_color', '#fff');
+$links_color = get_theme_mod('team_plugin_links_color','#278dec');
+
 
 
  ?>
