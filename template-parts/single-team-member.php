5d896b52addfcb51b7c72d5f43a252e87dbdd8ec
abaicus <baicusandrei@me.com>
2016-03-29 16:03:17 +0300

    !!!Styled single post page.

--- template-parts/single-team-member.php
+++ template-parts/single-team-member.php
@@ -5,126 +5,122 @@
  * @package team-plugin
  */
 
-get_header();
-?>
 
-<div class="content-wrap">
+ get_header(); ?>
+<div class="row">
+ 	<div id="primary" class="content-area col-md-12">
+ 		<main id="main" class="site-main" role="main">
 
-	<div id="primary" class="col-sm-12 col-md-9 content-area">
-		<main id="main" class="site-main" role="main">
+ 		<?php
+ 		while ( have_posts() ) : the_post(); ?>
 
-			<?php
-			while ( have_posts() ) : the_post();
-?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<div class="entry-content">
 
 					<?php if(!empty(get_the_post_thumbnail())) { ?>
 
-					<div class="lawyer-image">
-						<?php the_post_thumbnail('team-member-single-page-thumbnail'); ?>
-					</div>
-				<?php } ?>
+						<div class="team-member-image">
 
-					<div class="entry-content-lawyer">
-						<header class="entry-header">
-							<div class="entry-content-lawyer-top">
+							<?php the_post_thumbnail('team-member-single-page-thumbnail'); ?>
 
-								<?php the_title( '<h1 class="entry-title">', '</h1>' );
-
-								$role_meta = get_post_meta($post->ID,'role_text',true);
-
-								if(!empty($role_meta)) {
-
-									echo '<h3>'. $role_meta .'</h3>';
-
-								} ?>
-
-								<?php $social_meta = get_post_meta($post->ID,'social_icons',true);
-								if(!empty($social_meta)) {
-
-								echo '<p class="lawyer-social-media">';
-
-								foreach($social_meta as $social_icon) {
-
-										if( !empty($social_icon['title']) && !empty($social_icon['icons']) ) { ?>
-
-											<a href="<?php echo esc_html($social_icon['title']); ?>"><i class="fa <?php echo esc_html($social_icon['icons']);?>"></i></a>
-							<?php }
-									}
-									echo '</p>';
-								}?>
-
-							</div>
-						</header><!-- .entry-header -->
-
-						<?php
-							the_content();
-
-							wp_link_pages( array(
-								'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'lawyeriax' ),
-								'after'  => '</div>',
-							) );
-						?>
-					</div>
-				</div><!-- .entry-content -->
-
-
-
-
-				<div class="practice-aria-lawyers-page">
-
-					<?php $group_heading_meta = get_post_meta($post->ID,'group_heading_text',true);
-
-					if(!empty($group_heading_meta)) {
-
-						echo '<h3>' . esc_html($group_heading_meta) . '</h3>';
-
-					}?>
-
-					<?php
-					$terms = get_the_terms($post->ID, 'member-group');
-
-					if(!empty($terms)) {
-					?>
-
-						<div class="practice-aria-lawyers-inner">
-
-							<ul class="practice-aria-lawyer">
-
-								<?php
-
-								foreach ($terms as $term) {
-
-									echo '<li class="col-md-2"><a href="' . get_term_link($term) . '">' . esc_html($term->name) . '</a></li>';
-
-								} ?>
-
-							</ul>
-
-						</div><!-- practice-aria-lawyers-inner -->
+						</div>
 
 					<?php } ?>
 
-				</div><!-- practice-aria-lawyers-page -->
+					<header class="entry-header">
 
+						<?php the_title( '<h1 class="entry-title">', '</h1>' );
 
+						$role_meta = get_post_meta($post->ID, 'role_text', true);
 
-				<footer class="entry-footer">
-					<?php
-						edit_post_link(
-							sprintf(
-								/* translators: %s: Name of current post */
-								esc_html__( 'Edit %s', 'lawyeriax' ),
-								the_title( '<span class="screen-reader-text">"', '"</span>', false )
-							),
-							'<span class="edit-link">',
-							'</span>'
-						);
-					?>
-				</footer><!-- .entry-footer -->
-			</article><!-- #post-## -->
+						if(!empty($role_meta)) {
+
+							echo '<h3>'. $role_meta .'</h3>';
+
+						}
+
+						$social_meta = get_post_meta($post->ID,'social_icons',true);
+
+						if(!empty($social_meta)) {
+
+							echo '<p class="team-member-social-icons">';
+
+							foreach($social_meta as $social_icon) {
+
+									if( !empty($social_icon['title']) && !empty($social_icon['icons']) ) { ?>
+
+										<a href="<?php echo esc_html($social_icon['title']); ?>"><i class="fa <?php echo esc_html($social_icon['icons']);?>"></i></a>
+
+									<?php }
+							}
+
+								echo '</p>';
+
+						} ?>
+
+					</header>
+
+					<div class="entry-content">
+
+						<?php the_content();
+
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'team-plugin' ),
+							'after'  => '</div>',
+						)); ?>
+
+					</div> <!-- entry-content -->
+
+					<footer class="entry-footer">
+
+						<div class="team-member-groups">
+
+							<?php $group_heading_meta = get_post_meta($post->ID,'group_heading_text',true);
+
+							if(!empty($group_heading_meta)) {
+
+								echo '<h3>' . esc_html($group_heading_meta) . '</h3>';
+
+							}?>
+
+							<?php $terms = get_the_terms($post->ID, 'member-group');
+
+							if(!empty($terms)) {
+
+							?>
+
+									<ul class="group-team-member">
+
+										<?php
+
+										foreach ($terms as $term) {
+
+											echo '<li class="col-md-2"><a href="' . get_term_link($term) . '">' . esc_html($term->name) . '</a></li>';
+
+										} ?>
+
+									</ul>
+
+							<?php } ?>
+
+						</div><!-- team-member-groups -->
+
+						<?php
+							edit_post_link(
+								sprintf(
+									/* translators: %s: Name of current post */
+									esc_html__( 'Edit %s', 'lawyeriax' ),
+									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+								),
+								'<span class="edit-link">',
+								'</span>'
+							);
+						?>
+					</footer><!-- .entry-footer -->
+			</article>
+		</main><!-- #main -->
+	</div> <!-- #primary -->
+</div>
+
 
 <?php
 				// If comments are open or we have at least one comment, load up the comment template.
@@ -135,8 +131,9 @@ get_header();
 			endwhile; // End of the loop.
 			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
+</div><!-- row -->
 
 <?php
 get_footer();
